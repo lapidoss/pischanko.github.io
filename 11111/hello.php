2d8802a60b605b0147b60f19497eec40a2bbdb65
Anatoliy <pischanko@gmail.com>
2018-08-04 15:13:57 +0300

    Update hello.php

--- 11111/hello.php
+++ 11111/hello.php
@@ -6,9 +6,3 @@ session_start();
 $_SESSION['name'] = $_POST['name'];
 echo 'Привет, '.$_SESSION['name']."<br>";
 
-/*
-
-if(!isset($_SESSION['name'])) {
-    echo "Твой ник: " . $_SESSION['name'] . "<br>";
-
-}*/
