aad590ad493f30d210f1334a1975591fba652543
Anatoliy <pischanko@gmail.com>
2018-08-04 15:12:38 +0300

    Задание№2
    Создать сайт из четырех страниц. На четвертой странице пользователь видит список страниц, которые он посещал.

---
+++ 22222/index.php
@@ -0,0 +1,28 @@
+<?php
+    require_once "list.php";
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Сайт из 4х страниц</title>
+</head>
+<body>
+
+<div >
+    <ul>
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="page2.php">Страница №2</a></li>
+        <li><a href="page3.php">Станица №3</a></li>
+        <li><a href="page4.php">Список страниц</a></li>
+    </ul>
+
+</div>
+
+
+
+</body>
+</html>
